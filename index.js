ecafb3d99af4e24a625e6be9eee37af336ae6631
nbk133 <mouaadelouanan2008@gmail.com>
2025-02-24 21:28:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["𝙉𝗕𝙆✊🏿", "𝗖𝗼𝗺𝗶𝗻𝗴 𝗦𝗼𝗼𝗻...⏳"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
