760af4b3d40c3296fddb210d5ea7fb71eafe45ca
nbk33 <nabilaelmgammate@gmail.com>
2025-02-24 22:23:10 +0000

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["𝗖𝗼𝗺𝗶𝗻𝗴 𝗦𝗼𝗼𝗻...⏳","𝙉𝗕𝙆✊🏿"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
